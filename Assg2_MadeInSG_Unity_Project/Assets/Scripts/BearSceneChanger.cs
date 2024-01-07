9e61c6952746b92ee60f84e2254fc2914cc786b0
hisokabubble <s10222087@connect.np.edu.sg>
2024-01-07 23:56:16 +0800

    Final

--- Assg2_MadeInSG_Unity_Project/Assets/Scripts/BearSceneChanger.cs
+++ Assg2_MadeInSG_Unity_Project/Assets/Scripts/BearSceneChanger.cs
@@ -10,7 +10,7 @@ public class BearSceneChanger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(2);
         }
     }
 }
